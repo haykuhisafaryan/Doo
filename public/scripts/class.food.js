c176a69a1627956c5602a1baa984d188ab84d016
haykuhisafaryan <43002849+haykuhisafaryan@users.noreply.github.com>
2018-11-23 10:33:31 +0400

    Update class.food.js

--- public/scripts/class.food.js
+++ public/scripts/class.food.js
@@ -1,3 +1,14 @@
 class Food extends Parent{
+     constructor(x,y,width, height, image, numberoffoods){
+        super(x,y,width, height, image);
+        this.x = rand(cnv.width - foodWidth);
+        this.y =  rand(cnv.height - foodHeight);
+        
+    }
+
     
 }
+for (let i = 0; i< numeroffoods; i++){ 
+    foodArr.push(new Food (cnv.width/3, cnv.height/3, foodWidth, foodHeight,  foodImage))
+} 
+    
